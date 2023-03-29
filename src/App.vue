d1f1d104e45d4e5c693e30c5f462e199fb794180
D4riu5 <R3iji94@gmail.com>
2023-03-29 18:35:23 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import Component1 from './components/Component1.vue';
+import { store } from "./store.js";
+
+export default {
+    name: "App",
+    components: {
+      Component1,
+    },
+    data() {
+          return {
+             store, 
+          }
+      },
+    methods:{
+        
+    },
+};
+</script>
+
+
+<template>
+  <!-- placeholder -->
+  <Component1/> 
+  <!-- placeholder -->
+</template>
+
+
+<style lang="scss">
+@use "./styles/main";
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+// bootstrap css/icons
+import 'bootstrap/dist/css/bootstrap.min.css'
+// bootstrap components
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// fontawesome icons
+import '@fortawesome/fontawesome-free/css/all.css'
+
+// axios is installed, use import axios from 'axios', in the component when you need to use it
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
